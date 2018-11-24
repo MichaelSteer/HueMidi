c6ed9a72538b50fed01b09c2a6cca440b50a67fe
MichaelSteer <swedester44@gmail.com>
2018-11-24 00:54:57 -0700

    File structure and package configuration

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+def readme():
+    with open('Readme.md') as rd:
+        return rd.read()
+
+
+setup(name='MidiHue',
+      version='0.0',
+      description='Phillips Hue MIDI Control Library',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Audio Interfacing :: MIDI',
+      ],
+      keywords='MIDI Hue Phillips Audio Performance Lighting',
+      url='https://github.com/MichaelSteer/HueMidi',
+      author='Michael Steer',
+      author_email='steer@ualberta.ca',
+      license='MIT',
+      packages=['HueMidi'],
+      install_requires=[
+          'markdown',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={},
+      include_package_data=True,
+      zip_safe=False)
